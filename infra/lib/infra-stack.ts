54337330f9521491de7a8c837d285a6737d3f63d
Drew Kilb <drew.kilb@duke.edu>
2022-05-07 21:54:44 -0400

    feat: Add basic CDK definition for Lambda function + URL
    Using the AWS CDK, the stack defines a function with the Rust
    binary, a URL for the function, and necessary permissions. As
    something new, I'm trying a Lambda on ARM architecture, which will
    be cheaper.

---
+++ infra/lib/infra-stack.ts
@@ -0,0 +1,45 @@
+import {
+  aws_lambda as lambda,
+  CfnOutput,
+  CfnResource,
+  Stack,
+  StackProps,
+} from "aws-cdk-lib";
+import { Construct } from "constructs";
+
+export class InfraStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const alertLambda = new lambda.Function(this, "AlertHandler", {
+      runtime: lambda.Runtime.PROVIDED_AL2,
+      architecture: lambda.Architecture.ARM_64,
+      handler: "alertHandler",
+      code: lambda.Code.fromAsset("../lambdas/ics-alerts"),
+      memorySize: 256,
+    });
+
+    const fnUrl = new CfnResource(this, "alertFnUrl", {
+      type: "AWS::Lambda::Url",
+      properties: {
+        TargetFunctionArn: alertLambda.functionArn,
+        AuthType: "NONE",
+        Cors: { AllowOrigins: ["*"] },
+      },
+    });
+
+    const fnUrlPermission = new CfnResource(this, "fnUrlPermission", {
+      type: "AWS::Lambda::Permission",
+      properties: {
+        FunctionName: alertLambda.functionName,
+        Principal: "*",
+        Action: "lambda:InvokeFunctionUrl",
+        FunctionUrlAuthType: "NONE",
+      },
+    });
+
+    new CfnOutput(this, "funcUrl", {
+      value: `${fnUrl.getAtt("FunctionUrl")}`,
+    });
+  }
+}
